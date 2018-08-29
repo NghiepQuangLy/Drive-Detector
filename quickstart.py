f9e845f310f7537a1ced3fb199a8f31efe11a42c
Quang Nghiep Ly <qlyy0001@student.monash.edu>
2018-08-30 01:46:51 +1000

    printing revisions in formatted output

--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,8 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
+import json
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
 
@@ -27,10 +29,20 @@ def main():
     else:
         print('Files:')
         for item in items:
-            print(item['name'], item['id'], item['capabilities']['canReadRevisions'])
+            print("Name: ", item['name'], "\nID: ", item['id'])
+            print("------------------------------------------")
             if item['capabilities']['canReadRevisions'] is True:
-                revisions = service.revisions().list(fileId=item['id']).execute()
-                print(revisions)
+                revisions_json = service.revisions().list(fileId=item['id']).execute()
+                revisions_str = json.dumps(revisions_json['revisions'])
+                #print(revisions_json)
+                revisions_dict = json.loads(revisions_str)
+                print("Revision ID - Modified time")
+                for revision in revisions_dict:
+                    print('{0} - {1}'.format(revision.get('id'), revision.get('modifiedTime')))
+                    #print("Last modified user: ", revision.get('lastModifyingUser').get('displayName'), revision.get('lastModifyingUser').get('emailAddress'), "\n")
+            else:
+                print("Can not read revision - NO PERMISSION")
+            print()
 
 if __name__ == '__main__':
     main()
