b3073e0142706ea732bf0f773d740ac82304b4ab
Quang Nghiep Ly <qlyy0001@student.monash.edu>
2018-09-13 01:44:17 +1000

    refactor code to make variable names and references easier to understand

--- main_program.py
+++ main_program.py
@@ -12,27 +12,37 @@ SCOPES_REST = 'https://www.googleapis.com/auth/drive.readonly'
 # If modifying these scopes, delete the file token_ACTIVITY.json.
 SCOPES_ACTIVITY = 'https://www.googleapis.com/auth/activity'
 
+class API:
+    def __init__(self, token, scope, name, version, api_name):
+        self.token = token
+        self.scope = scope
+        self.name = name
+        self.version = version
+        self.api_name = api_name
+
 def main():
 
-    store = file.Storage('token_REST.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES_REST)
-        creds = tools.run_flow(flow, store)
-    service_REST = build('drive', 'v3', http=creds.authorize(Http()))
+    apis = [API('token_REST.json',     SCOPES_REST,     'drive',        'v3', 'rest'),
+            API('token_ACTIVITY.json', SCOPES_ACTIVITY, 'appsactivity', 'v1', 'activity')]
 
-    store = file.Storage('token_ACTIVITY.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES_ACTIVITY)
-        creds = tools.run_flow(flow, store)
-    service_ACTIVITY = build('appsactivity', 'v1', http=creds.authorize(Http()))
+    services = {}
 
-    # Prints all the files in a team drive as well as their revisions
-    drive_id = drive_info(service_REST)
+    for api in apis:
+        store = file.Storage(api.token)
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('credentials.json', api.scope)
+            creds = tools.run_flow(flow, store)
+        services[api.api_name] = build(api.name, api.version, http=creds.authorize(Http()))
 
-    # Prints all activities in a team drive
-    user_activity_info(service_ACTIVITY, drive_id)
+    try:
+        # Prints all the files in a team drive as well as their revisions
+        drive_id = drive_info(services['rest'])
+
+        # Prints all activities in a team drive
+        user_activity_info(services['activity'], drive_id)
+    except:
+        print("An error happened")
 
 if __name__ == '__main__':
     main()
