fd8363840c972bb7847450e5313bc11f38c538eb
Quang Nghiep Ly <qlyy0001@student.monash.edu>
2018-09-13 11:45:45 +1000

    further refactoring and changes are now categorised into the file that was changed

--- activity_api.py
+++ activity_api.py
@@ -1,26 +1,31 @@
 import datetime
 
-def main(service, drive_id):
+def get_changes(service, file_id):
     """
     Prints information about the last 10 events that occurred the user's Drive.
     """
 
     # Call the Drive Activity API
-    results = service.activities().list(source='drive.google.com',
-        drive_ancestorId=drive_id, pageSize=10).execute()
+    results = service.activities().list(source='drive.google.com', drive_fileId=file_id, pageSize=20).execute()
     activities = results.get('activities', [])
+    return activities
 
-    if not activities:
-        print('No activity.')
+def print_changes(changes):
+    print('\t------------------------------------------')
+
+    if not changes:
+        print('\tNo activity.')
     else:
-        print('Recent activity:')
-        for activity in activities:
-            event = activity['combinedEvent']
+        print('\t Recent activity:')
+        print('\t', '{:30} {:25} {:10}'.format("Time", "User", "Action"))
+        for change in changes:
+            event = change['combinedEvent']
             user = event.get('user', None)
             target = event.get('target', None)
             if user is None or target is None:
                 continue
             time = datetime.datetime.fromtimestamp(
                 int(event['eventTimeMillis'])/1000)
-            print('{0}: {1}, {2}, {3} ({4})'.format(time, user['name'],
-                event['primaryEventType'], target['name'], target['mimeType']))
+            print('\t', time, '    {:25} {:10}'.format(user['name'], event['primaryEventType']))
+
+    print()
--- main_program.py
+++ main_program.py
@@ -3,8 +3,8 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-from rest_api import main as drive_info
-from activity_api import main as user_activity_info
+import rest_api
+import activity_api
 
 # If modifying these scopes, delete the file token_REST.json.
 SCOPES_REST = 'https://www.googleapis.com/auth/drive.readonly'
@@ -20,7 +20,20 @@ class API:
         self.version = version
         self.api_name = api_name
 
+def get_service(api):
+
+    store = file.Storage(api.token)
+    creds = store.get()
+
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', api.scope)
+        creds = tools.run_flow(flow, store)
+
+    return build(api.name, api.version, http=creds.authorize(Http()))
+
 def main():
+    # Prints all the files in a team drive as well as their revisions
+    # Prints all activities in a team drive
 
     apis = [API('token_REST.json',     SCOPES_REST,     'drive',        'v3', 'rest'),
             API('token_ACTIVITY.json', SCOPES_ACTIVITY, 'appsactivity', 'v1', 'activity')]
@@ -28,23 +41,50 @@ def main():
     services = {}
 
     for api in apis:
-        store = file.Storage(api.token)
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets('credentials.json', api.scope)
-            creds = tools.run_flow(flow, store)
-        services[api.api_name] = build(api.name, api.version, http=creds.authorize(Http()))
+        services[api.api_name] = get_service(api)
 
-    try:
-        # Prints all the files in a team drive as well as their revisions
-        drive_id = drive_info(services['rest'])
+    drive_id = None
 
-        # Prints all activities in a team drive
-        user_activity_info(services['activity'], drive_id)
-    except:
-        print("An error happened")
+    # get all team drives of user
+    team_drives = rest_api.get_team_drives(services['rest'])
+
+    # prompts the user for a team drive name
+    user_input_team_drive = input("Please enter the name of a team drive: ")
+
+    # if the user has no team drives
+    if not team_drives:
+        print('No drives found')
+        return
+    else:
+
+        # traverse all the team drives the user has until the one specified by the user is found
+        found = False
+        for drive in team_drives:
+            if drive['name'] == user_input_team_drive:
+                found = True
+                break
+
+        # if the team drive by the specified by the user does not exist
+        if not found:
+            print("No such drive")
+            return drive_id
+
+    print('D R I V E:', drive['name'])
+
+    print('*********************************')
+
+    files = rest_api.get_files(services['rest'], drive)
+
+    if not files:
+        print('No files found')
+    else:
+        print('F I L E S:')
+        for a_file in files:
+            rest_api.print_file_all_info(services['rest'], a_file)
+            file_changes = activity_api.get_changes(services['activity'], a_file['id'])
+            activity_api.print_changes(file_changes)
+
+    print('*********************************')
 
 if __name__ == '__main__':
     main()
-
-
