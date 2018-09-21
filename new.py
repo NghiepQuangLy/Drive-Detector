3a06bf2f055086204da7f260d7d403f1d325f78b
Quang Nghiep Ly <qlyy0001@student.monash.edu>
2018-09-21 15:29:16 +1000

    staging deleted files in git and commit

--- new.py
+++
@@ -1,4 +0,0 @@
-tags = ['{:21}'.format('Name:'),'{:21}'.format('ID:'),'{:21}'.format('Trashed:'),'{:21}'.format('Last Modifying User:')]
-values = ['hello world', '18', 'True', 'Mike']
-separator=', '
-print(separator.join(values))
