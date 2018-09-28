3cae2c70cb22402f50f92eba8a811033dcd02477
Manvendra Singh <msin0017@student.monash.edu>
2018-09-28 21:08:53 +1000

    Changed look , Stacked the histogram and made it horizontal.
    The look was changed to a more modern look as the client desired. The layout was also changed according what the client wanted by stacking and making the bars horizontal.

--- histogram.py
+++ histogram.py
@@ -28,6 +28,7 @@ legend = ['Jack', 'Manvendra', 'Mike', 'Vibhas', 'Tito']
 #number of users in team_drive
 N = 5
 
+
 #creating a subplot (multiple bar graphs in one)
 fig, ax = plt.subplots()
 
@@ -36,7 +37,7 @@ fig, ax = plt.subplots()
 ind = np.arange(N)
 
 #width of each individual bar
-width = 0.15  
+width = 0.50
 
 #INSERTIONS
 #--------------------------------------------------------------------------------
@@ -44,7 +45,7 @@ width = 0.15
 insertionsCount = [43, 28, 74, 62, 12] 
 
 #create the first bar which represents insertions, coloured red
-insertions = ax.bar(ind, insertionsCount, width, color='r', bottom=0, label = 'Insertions')
+insertions = ax.barh(ind, insertionsCount, width, color='#b5ffb9', left=None, label = 'Insertions')
 
 
 #DELETIONS
@@ -53,7 +54,7 @@ insertions = ax.bar(ind, insertionsCount, width, color='r', bottom=0, label = 'I
 deletionsCount = [193, 29, 78, 165, 200]
 
 #create the second bar which represents deletions, coloured yellow
-deletions = ax.bar(ind + width, deletionsCount, width, color='y', bottom=0, label = 'Deletions')
+deletions = ax.barh(ind , deletionsCount, width, color='#f9bc86', left=insertionsCount, label = 'Deletions')
 
 #COMMENTS
 #---------------------------------------------------------------------------------
@@ -61,36 +62,53 @@ deletions = ax.bar(ind + width, deletionsCount, width, color='y', bottom=0, labe
 commentsCount = [15, 21, 18, 9, 26]
 
 #create the third bar which represents comments, coloured blue
-comments = ax.bar(ind + (2*width), commentsCount, width, color='b', bottom=0, label = 'Comments')
+bottom_start = deletionsCount+insertionsCount
+comments = ax.barh(ind , commentsCount, width, color='#a3acff', left=np.array(insertionsCount)+np.array(deletionsCount), label = 'Comments')
 
 #EDITS
 #---------------------------------------------------------------------------------
+
 editsCount = [insertionsCount[0] + deletionsCount[0] + commentsCount[0], insertionsCount[1] + deletionsCount[1] + commentsCount[1], insertionsCount[2] + deletionsCount[2] + commentsCount[2], insertionsCount[3] + deletionsCount[3] + commentsCount[3], insertionsCount[4] + deletionsCount[4] + commentsCount[4]]
 
-edits = ax.bar(ind + 3*(width), editsCount, width, color='g', bottom=0, label = 'Total Edits')
+#edits = ax.bar(ind + 3*(width), editsCount, width, color='g', bottom=0, label = 'Total Edits')
 
 #set the axis title
 ax.set_title('Actions per user for ' + fileName + ' in team_drive' + teamDrive)
 
 #set where the x labels will be (centred in the middle of all 4 columns)
-ax.set_xticks(ind + width + width / 2)
+ax.set_yticks(ind)
 
 #label these x-axis labels
-ax.set_xticklabels(('Jack', 'Manvendra', 'Mike', 'Vibhas', 'Tito'))
+ax.set_yticklabels(('Jack', 'Manvendra', 'Mike', 'Vibhas', 'Tito'))
 
 #create the legend
 #ax.legend((insertions[0], deletions[0], comments[0], edits[0]), ('Insertion', 'Deletion', 'Comments', 'Total Edits'))
 ax.legend()
 #autoscale the axes based on the data that is fed into the insertions/deletions/comments/edit lists
 ax.autoscale_view()
+"""""
+xpos='center'
+xpos = xpos.lower()  # normalize the case of the parameter
+ha = {'center': 'center', 'right': 'left', 'left': 'right'} #jack vibhas tito mike man
+offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
+ax.text(ind[0] , 1.01 * editsCount[0],
+            '{}'.format(editsCount[0]), ha=ha[xpos], va='bottom')
+ax.text(ind[1] , 1.01 * editsCount[1],
+            '{}'.format(editsCount[1]), ha=ha[xpos], va='bottom')
+ax.text(ind[2] , 1.01 * editsCount[2],
+            '{}'.format(editsCount[2]), ha=ha[xpos], va='bottom')
+"""""
+
+
+"""
 
 def autolabel(rects, xpos='center'):
-    """
+    """"""
     Attach a text label above each bar in *rects*, displaying its height.
 
     *xpos* indicates which side to place the text w.r.t. the center of
     the bar. It can be one of the following {'center', 'right', 'left'}.
-    """
+    """"""
 
     xpos = xpos.lower()  # normalize the case of the parameter
     ha = {'center': 'center', 'right': 'left', 'left': 'right'}
@@ -104,8 +122,12 @@ def autolabel(rects, xpos='center'):
 #positioning the text label to display individual bar height
 autolabel(insertions, "center")
 autolabel(deletions, "center")
-autolabel(edits,"center")
+#autolabel(edits,"center")
 autolabel(comments,"center")
+
+"""
+
+
 #DISPLAY the graph
 fig = plt.figure()
 mpld3.save_html(fig, "test1.html")
