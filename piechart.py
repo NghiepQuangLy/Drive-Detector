e0f21d6213daa0709be0939e9847a3ccb77c311e
JackBeard <jbea0002@student.monash.edu>
2018-10-02 21:06:27 +1000

    PIECHART (total number of actions) Added random colour selection if there are multiple users // Made compatible for output from main_program.py // First test file
    PER FILE, next version will be for an entire drive

--- piechart.py
+++ piechart.py
@@ -13,29 +13,37 @@
 #3. If this doesn't work, I recommend either reinstalling a later version of python which has pip installed. Otherwise you can just try to
 # install pip separately, then try. This didn't work with my python 3.6 at home, but worked with python 3.7.
 
-
+from main_program import *
 import matplotlib.pyplot as plt
+from random import randint
+
 
 #ignore this, was just for testing random values
 import numpy as np
 np.random.seed(444)
 
-#LIST to hold the integer data (whatever it is we want that to be, for each pie chart)
-slices_numberOfRevisions = [69, 24, 36, 74, 109]
+def create_pie_chart(drive_name, file_name, users, actions):
+    #LIST to hold the integer data (whatever it is we want that to be, for each pie chart)
+    slices_numberOfRevisions = actions
 
-#LIST to hold the category data (whatever it is we want that to be, for each pie chart)
-users = ['Jack', 'Manvendra', 'Mike', 'Vibhas', 'Tito']
+    #LIST to hold the category data (whatever it is we want that to be, for each pie chart)
+    contributors = users
 
-#LIST to hold colours for each modifying user
-colors = ['r', 'g', 'b', 'c', 'm']
+    #LIST to hold colours for each modifying user
+    all_colors = ['b', 'g', 'r', 'c', 'm', 'y'] 
+    colors = [all_colors[randint(0,5)] for i in range(len(contributors))]
 
-#CALL the pie function from the api which takes the integer data, category data, colour data, starting angle of the pie chart segments (this doesn't matter), then autopct puts labels on the pie chart segments if it's wanted)
-plt.pie(slices_numberOfRevisions, labels=users, colors=colors, startangle=90, autopct='%.1f%%')
+    #CALL the pie function from the api which takes the integer data, category data, colour data, starting angle of the pie chart segments (this doesn't matter), then autopct puts labels on the pie chart segments if it's wanted)
+    plt.pie(slices_numberOfRevisions, labels=contributors, colors=colors, startangle=90, autopct='%.1f%%')
 
-plt.title('Number of revisions per modifying user')
-           
-#DISPLAY the pie chart
-plt.show()
+    plt.title('Total Actions Per User For File ' + file_name + ' In Drive ' + drive_name)
+                   
+    #DISPLAY the pie chart
+    plt.show()
+
+    return plt
+
+create_pie_chart('FIT2101', 'testFile', ['yeeeeep', 'nahhhh', 'woooo'], [1, 4, 2])
 
 #------------------------------------------------------------------------------------------------
 
